e6b94cd66e7f02e89b59439927acedfbcf75feb7
fazza_abiyyu <1700428480@qq.com>
2024-12-25 11:27:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,13 +11,29 @@ def predict():
     end_year = request.args.get('end_year', type=int)
     
     if not start_year or not end_year:
-        return jsonify({'error': 'Please provide both start_year and end_year as query parameters'}), 400
+        return jsonify({'code': 400, 'message': 'Please provide both start_year and end_year as query parameters', 'data': {}}), 400
 
     result = train_and_predict(start_year, end_year)
     if result:
-        return jsonify(result)
+        data = {
+            "prediksi": [
+                {"Tahun Prediksi": end_year + 1},
+                {"name": "Laki - Laki", "data": [pred['num_pred_male'] for pred in result['monthly_predictions']]},
+                {"name": "Perempuan", "data": [pred['num_pred_female'] for pred in result['monthly_predictions']]}
+            ],
+            "categories": [
+                "Januari", "Februari", "Maret", "April", "Mei", "Juni",
+                "Juli", "Agustus", "September", "Oktober", "November", "Desember"
+            ]
+        }
+        response = {
+            "code": 200,
+            "message": "Data berhasil dikembalikan",
+            "data": data
+        }
+        return jsonify(response)
     else:
-        return jsonify({'error': 'No stunting data available for the specified years'}), 400
+        return jsonify({'code': 400, 'message': 'No stunting data available for the specified years', 'data': {}}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
